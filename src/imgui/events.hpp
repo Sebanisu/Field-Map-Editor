c1181b3ac18c2ec3a6b57eb10e709bee24210108
Sebanisu <peeceseven@gmail.com>
2021-09-06 21:32:39 -0400

    I wanted a variant for event handling.

--- src/imgui/events.hpp
+++ src/imgui/events.hpp
@@ -5,90 +5,98 @@
 #ifndef MYPROJECT_EVENTS_HPP
 #define MYPROJECT_EVENTS_HPP
 #include <SFML/Window/Event.hpp>
+#include <utility>
+#include <variant>
+
 namespace events {
-std::pair<sf::Event::EventType,
-  std::variant<std::monostate,
-    sf::Event::SizeEvent,
-    sf::Event::TextEvent,
-    sf::Event::KeyEvent,
-    sf::Event::MouseWheelEvent,
-    sf::Event::MouseWheelScrollEvent,
-    sf::Event::MouseButtonEvent,
-    sf::Event::MouseMoveEvent,
-    sf::Event::JoystickButtonEvent,
-    sf::Event::JoystickMoveEvent,
-    sf::Event::JoystickConnectEvent,
-    sf::Event::TouchEvent,
-    sf::Event::SensorEvent>>
-  get(const sf::Event &event)
+template<typename... B>
+[[nodiscard]] inline auto make_visitor(B &&...b) noexcept
+{
+  struct visitor : public std::decay_t<B>...
+  {
+    using B::operator()...;
+  };
+  return visitor{ std::forward<B>(b)... };
+}
+using sf_event_variant = std::variant<std::monostate,
+  sf::Event::SizeEvent,
+  sf::Event::TextEvent,
+  sf::Event::KeyEvent,
+  sf::Event::MouseWheelEvent,
+  sf::Event::MouseWheelScrollEvent,
+  sf::Event::MouseButtonEvent,
+  sf::Event::MouseMoveEvent,
+  sf::Event::JoystickButtonEvent,
+  sf::Event::JoystickMoveEvent,
+  sf::Event::JoystickConnectEvent,
+  sf::Event::TouchEvent,
+  sf::Event::SensorEvent>;
+[[nodiscard]] inline sf_event_variant
+  get(const sf::Event &event) noexcept
 {
   switch (event.type) {
   case sf::Event::EventType::Closed:
     ///< The window requested to be closed (no data)
-    return { event.type, std::monostate{} };
-  case sf::Event::EventType::Resized:
-    ///< The window was resized (data in event.size)
-    return { event.type, event.size };
   case sf::Event::EventType::LostFocus:
     ///< The window lost the focus (no data)
-    return { event.type, std::monostate{} };
   case sf::Event::EventType::GainedFocus:
     ///< The window gained the focus (no data)
-    return { event.type, std::monostate{} };
+  case sf::Event::EventType::MouseEntered:
+    ///< The mouse cursor entered the area of the window (no data)
+  case sf::Event::EventType::MouseLeft:
+    ///< The mouse cursor left the area of the window (no data)
+  case sf::Event::EventType::Count:
+    ///< Keep last -- the total number of event types
+  default:
+    return std::monostate{};
+  case sf::Event::EventType::Resized:
+    ///< The window was resized (data in event.size)
+    return event.size;
   case sf::Event::EventType::TextEntered:
     ///< A character was entered (data in event.text)
-    return { event.type, event.text };
+    return event.text;
   case sf::Event::EventType::KeyPressed:
     ///< A key was pressed (data in event.key)
   case sf::Event::EventType::KeyReleased:
     ///< A key was released (data in event.key)
-    return { event.type, event.key };
+    return event.key;
   case sf::Event::EventType::MouseWheelMoved:
     ///< The mouse wheel was scrolled (data in event.mouseWheel) (deprecated)
-    return { event.type, event.mouseWheel };
+    return event.mouseWheel;
   case sf::Event::EventType::MouseWheelScrolled:
     ///< The mouse wheel was scrolled (data in event.mouseWheelScroll)
-    return { event.type, event.mouseWheelScroll };
+    return event.mouseWheelScroll;
   case sf::Event::EventType::MouseButtonPressed:
     ///< A mouse button was pressed (data in event.mouseButton)
   case sf::Event::EventType::MouseButtonReleased:
     ///< A mouse button was released (data in event.mouseButton)
-    return { event.type, event.mouseButton };
+    return event.mouseButton;
   case sf::Event::EventType::MouseMoved:
     ///< The mouse cursor moved (data in event.mouseMove)
-    return { event.type, event.mouseMove };
-  case sf::Event::EventType::MouseEntered:
-    ///< The mouse cursor entered the area of the window (no data)
-    return { event.type, std::monostate{} };
-  case sf::Event::EventType::MouseLeft:
-    ///< The mouse cursor left the area of the window (no data)
-    return { event.type, std::monostate{} };
+    return event.mouseMove;
   case sf::Event::EventType::JoystickButtonPressed:
     ///< A joystick button was pressed (data in event.joystickButton)
   case sf::Event::EventType::JoystickButtonReleased:
     ///< A joystick button was released (data in event.joystickButton)
-    return { event.type, event.joystickButton };
+    return event.joystickButton;
   case sf::Event::EventType::JoystickMoved:
     ///< The joystick moved along an axis (data in event.joystickMove)
-    return { event.type, event.joystickMove };
+    return event.joystickMove;
   case sf::Event::EventType::JoystickConnected:
     ///< A joystick was connected (data in event.joystickConnect)
   case sf::Event::EventType::JoystickDisconnected:
     ///< A joystick was disconnected (data in event.joystickConnect)
-    return { event.type, event.joystickConnect };
+    return event.joystickConnect;
   case sf::Event::EventType::TouchBegan:
     ///< A touch event began (data in event.touch)
   case sf::Event::EventType::TouchMoved:
     ///< A touch moved (data in event.touch)
   case sf::Event::EventType::TouchEnded:
     ///< A touch event ended (data in event.touch)
-    return { event.type, event.touch };
+    return event.touch;
   case sf::Event::EventType::SensorChanged:
     ///< A sensor value changed (data in event.sensor)
-    return { event.type, event.sensor };
-  case sf::Event::EventType::Count:
-    ///< Keep last -- the total number of event types
-    return { event.type, std::monostate{} };
+    return event.sensor;
   }
 }
 };// namespace events
--- src/imgui/test.cpp
+++ src/imgui/test.cpp
@@ -1,26 +1,27 @@
 #include "archives_group.hpp"
 #include "dialog.hpp"
+#include "events.hpp"
 #include "filebrowser.hpp"
-#include "imgui_format_text.hpp"
-#include "mim_sprite.hpp"
 #include "open_viii/graphics/background/Map.hpp"
 #include <imgui-SFML.h>
 #include <imgui.h>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
-#include "events.hpp"
-#include <utility>
 
-void Game();
 void update_path(std::vector<std::string> &paths,
   int                                      current_map,
   int                                      path_selected_item,
   archives_group                          &opt_archives,
   open_viii::archive::FIFLFS<false>       &field,
   mim_sprite                              &ms,
-  bool                                    &changed);
-int  main() { Game(); }
+  bool                                    &changed)
+{
+  opt_archives = opt_archives.with_path(paths.at(path_selected_item));
+  field        = opt_archives.field(current_map);
+  ms           = ms.with_field(field);
+  changed      = true;
+}
+
 void Game()
 {
   // create a file browser instance
@@ -41,80 +42,68 @@ void Game()
     std::back_inserter(paths_c_str),
     [](const std::string &p) { return p.c_str(); });
 
-  auto                  opt_archives  = archives_group({}, paths.front());
-  int                   current_map   = 0;
-  auto                  field         = opt_archives.field(current_map);
-  auto                  ms            = mim_sprite(field, 4_bpp, 0, {});
-  static constexpr auto window_width  = 800;
-  static constexpr auto window_height = 600;
-  sf::RenderWindow      window(
-         sf::VideoMode(window_width, window_height), "ImGui + SFML = <3");
+  auto                    opt_archives  = archives_group({}, paths.front());
+  int                     current_map   = 0;
+  auto                    field         = opt_archives.field(current_map);
+  auto                    ms            = mim_sprite(field, 4_bpp, 0, {});
+  static constexpr auto   window_width  = 800;
+  static constexpr auto   window_height = 600;
+  sf::RenderWindow window(
+    sf::VideoMode(window_width, window_height), "ImGui + SFML = <3");
   window.setFramerateLimit(360U);
   ImGui::SFML::Init(window);
   sf::Clock  deltaClock;
   const auto original_style = ImGui::GetStyle();
   bool       first          = true;
-  auto       scale_window   = [&ms,
-                        &original_style,
-                        &window,
-                        save_width  = float{},
-                        save_height = float{}](
-                        float width = 0, float height = 0) mutable {
-    auto load = [](auto &saved, auto &not_saved) {
-      if (not_saved == 0) {
-        not_saved = saved;
-      } else {
-        saved = not_saved;
+  auto       scale_window =
+    [&ms, &original_style, save_width = float{}, save_height = float{}, &window](
+      float width = 0, float height = 0) mutable {
+      auto load = [](auto &saved, auto &not_saved) {
+        if (not_saved == 0) {
+          not_saved = saved;
+        } else {
+          saved = not_saved;
+        }
+      };
+      load(save_width, width);
+      load(save_height, height);
+      // this scales up the elements without losing the horizontal space. so
+      // going from 4:3 to 16:9 will end up with wide screen.
+      auto       scale       = width / window_height;
+      const auto scale_width = width / height * static_cast<float>(ms.height());
+      if (scale < 1.0F) {
+        scale = 1.0F;
       }
+      ImGui::GetIO().FontGlobalScale = std::round(scale);
+      ImGui::GetStyle() =
+        original_style;// restore original before applying scale.
+      ImGui::GetStyle().ScaleAllSizes(std::round(scale));
+      window.setView(sf::View(sf::FloatRect(
+        0.0F, 0.0F, scale_width, static_cast<float>(ms.height()))));
     };
-    load(save_width, width);
-    load(save_height, height);
-    // this scales up the elements without losing the horizontal space. so
-    // going from 4:3 to 16:9 will end up with wide screen.
-    auto       scale       = width / window_height;
-    const auto scale_width = width / height * static_cast<float>(ms.height());
-    if (scale < 1.0F) {
-      scale = 1.0F;
-    }
-    ImGui::GetIO().FontGlobalScale = std::round(scale);
-    ImGui::GetStyle() =
-      original_style;// restore original before applying scale.
-    ImGui::GetStyle().ScaleAllSizes(std::round(scale));
-    window.setView(sf::View(
-      sf::FloatRect(0.0F, 0.0F, scale_width, static_cast<float>(ms.height()))));
-  };
   if (window.isOpen()) {
     scale_window(
       static_cast<float>(window_width), static_cast<float>(window_height));
   }
   sf::Event event{};
-  while (true) {
-    if (!window.isOpen()) {
-      break;
-    }
-    while (true) {
-      if(!window.pollEvent(event)) {
-        break;
-      }
+  while (window.isOpen()) {
+    while (window.pollEvent(event)) {
       ImGui::SFML::ProcessEvent(event);
-      if (event.type == sf::Event::Resized) {
-        [&scale_window](sf::Event::SizeEvent size) {
-          scale_window(
-            static_cast<float>(size.width), static_cast<float>(size.height));
-        }(event.size);
-      } else if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+      const auto event_variant = events::get(event);
+      std::visit(events::make_visitor(
+                   [&scale_window](const sf::Event::SizeEvent &size) {
+                     scale_window(static_cast<float>(size.width),
+                       static_cast<float>(size.height));
+                   },
+                   [&event, &window](const auto) {
+                     if (event.type == sf::Event::Closed) {
+                       window.close();
+                     }
+                   }),
+        event_variant);
     }
     const auto view = window.getView();
-    // const auto view_port = window.getViewport(view);
     ImGui::SFML::Update(window, deltaClock.restart());
-    //    static constexpr float f2           = 2.0F;
-    //    const auto             shape_bounds = shape.getLocalBounds();
-    //    shape.setOrigin(shape_bounds.left + shape_bounds.width / f2,
-    //      shape_bounds.top + shape_bounds.height / f2);
-    //    shape.setPosition(view.getCenter());
-
     const static auto hello_world = dialog(
       "Hello, world!",
       ImVec2{ 0.0F, 0.0F },
@@ -195,16 +184,5 @@ void Game()
 
   ImGui::SFML::Shutdown();
 }
-void update_path(std::vector<std::string> &paths,
-  int                                      current_map,
-  int                                      path_selected_item,
-  archives_group                          &opt_archives,
-  open_viii::archive::FIFLFS<false>       &field,
-  mim_sprite                              &ms,
-  bool                                    &changed)
-{
-  opt_archives = opt_archives.with_path(paths.at(path_selected_item));
-  field        = opt_archives.field(current_map);
-  ms           = ms.with_field(field);
-  changed      = true;
-}
+
+int main() { Game(); }
