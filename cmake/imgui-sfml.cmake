56444f9662aa9bf7233dd6c55a752eb91fdba525
Sebanisu <peeceseven@gmail.com>
2021-10-04 12:34:50 -0400

    fix imgui-sfml to compile from src.

---
+++ cmake/imgui-sfml.cmake
@@ -0,0 +1,32 @@
+include(FetchContent)
+find_package(imgui REQUIRED)
+find_package(sfml REQUIRED)
+#list(GET imgui_INCLUDE_DIRS 0 IMGUI_DIR)
+#message(STATUS "dir='${IMGUI_DIR}'")
+#foreach(dir ${imgui_INCLUDE_DIRS})
+#    message(STATUS "dir='${dir}'")
+#endforeach()
+#options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
+FetchContent_Declare(
+imgui-sfml_fetch
+GIT_REPOSITORY https://github.com/eliasdaler/imgui-sfml.git
+GIT_TAG origin/master
+GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+)
+#FetchContent_MakeAvailable(imgui-sfml_fetch)
+FetchContent_GetProperties(imgui-sfml_fetch)
+if (NOT imgui-sfml_fetch_POPULATED)
+FetchContent_Populate(imgui-sfml_fetch)
+#add_subdirectory(${imgui-sfml_fetch_SOURCE_DIR} ${imgui-sfml_fetch_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_library(ImGui-SFML STATIC
+            ${imgui-sfml_fetch_SOURCE_DIR}/imgui-SFML.cpp
+            )
+    target_include_directories(ImGui-SFML
+            PUBLIC ${imgui-sfml_fetch_SOURCE_DIR}
+            )
+    target_link_libraries(ImGui-SFML
+            PUBLIC imgui::imgui
+            PUBLIC sfml-graphics
+            PUBLIC sfml-system
+            PUBLIC sfml-window)
+endif ()
