29e91b2ce60c823a687386f13677f326ed118a9c
Sebanisu <peeceseven@gmail.com>
2021-11-18 16:37:28 -0500

    episode 3

---
+++ src/imgui/opengl/episode_3/main.cpp
@@ -0,0 +1,67 @@
+#include <array>
+#include <fmt/format.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int
+  main(void)
+{
+  GLFWwindow *window;
+
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
+
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    fmt::print(
+      stderr,
+      "Error! {}:{} GLFW Window Failed to create\n",
+      __FILE__,
+      __LINE__);
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* Init GLEW after context */
+  if (glewInit() != GLEW_OK)
+  {
+    fmt::print(stderr, "Error! {}:{} GLEW NOT OKAY\n", __FILE__, __LINE__);
+  }
+
+  fmt::print("{}\n", glGetString(GL_VERSION));
+
+  std::array    positions{ -0.5F, -.5F, 0.F, 0.5F, 0.5F, -0.5F };
+
+  std::uint32_t buffer{};
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    std::size(positions) * sizeof(float),
+    std::data(positions),
+    GL_STATIC_DRAW);
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, std::size(positions) / 3U);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
