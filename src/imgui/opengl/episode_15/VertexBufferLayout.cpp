d5f35b0fd2096e073dbbcd612fb018ef31caa765
Sebanisu <peeceseven@gmail.com>
2021-11-23 15:51:16 -0500

    start episode 15

---
+++ src/imgui/opengl/episode_15/VertexBufferLayout.cpp
@@ -0,0 +1,37 @@
+//
+// Created by pcvii on 11/22/2021.
+//
+
+#include "VertexBufferLayout.hpp"
+
+std::size_t
+  VertexBufferLayout::VertexBufferElement::size_of_type(std::uint32_t type)
+{
+  switch (type)
+  {
+  case GL_FLOAT:
+    return sizeof(GLfloat);
+  case GL_UNSIGNED_INT:
+    return sizeof(GLuint);
+  case GL_BYTE:
+    return sizeof(GLbyte);
+  }
+  assert(false);
+  return 0;
+}
+std::size_t
+  VertexBufferLayout::VertexBufferElement::size() const
+{
+  return count * size_of_type(type);
+}
+
+const std::vector<VertexBufferLayout::VertexBufferElement> &
+  VertexBufferLayout::elements() const noexcept
+{
+  return m_elements;
+}
+std::size_t
+  VertexBufferLayout::stride() const noexcept
+{
+  return m_stride;
+}
