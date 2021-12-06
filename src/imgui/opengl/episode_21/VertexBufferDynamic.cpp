3eeaf9769e627209f4e9eb171ccef9b9424c679d
Sebanisu <peeceseven@gmail.com>
2021-12-06 05:53:54 -0500

    Changed vertex_buffer_dynamic to use unique_value

--- src/imgui/opengl/episode_21/VertexBufferDynamic.cpp
+++ src/imgui/opengl/episode_21/VertexBufferDynamic.cpp
@@ -1,23 +1,4 @@
 #include "VertexBufferDynamic.hpp"
-VertexBufferDynamic::~VertexBufferDynamic()
-{
-  GLCall{ glDeleteBuffers, 1, &m_renderer_id };
-  UnBind();
-}
-
-VertexBufferDynamic::VertexBufferDynamic(VertexBufferDynamic &&other) noexcept
-  : VertexBufferDynamic()
-{
-  swap(*this, other);
-}
-
-VertexBufferDynamic &
-  VertexBufferDynamic::operator=(VertexBufferDynamic &&other) noexcept
-{
-  swap(*this, other);
-  return *this;
-}
-
 void
   VertexBufferDynamic::Bind() const
 {
@@ -31,26 +12,22 @@ void
 }
 VertexBufferDynamic::VertexBufferDynamic(size_t count)
   : m_max_size(count * 4U)
+  , m_renderer_id{ [&count]() -> std::uint32_t
+                   {
+                     std::uint32_t tmp;
+                     GLCall{ glGenBuffers, 1, &tmp };
+                     GLCall{ glBindBuffer, GL_ARRAY_BUFFER, tmp };
+                     GLCall{ glBufferData,
+                             GL_ARRAY_BUFFER,
+                             static_cast<std::ptrdiff_t>(count * sizeof(Quad)),
+                             nullptr,
+                             GL_DYNAMIC_DRAW };
+                     return tmp;
+                   }(),
+                   [](std::uint32_t id)
+                   {
+                     GLCall{ glDeleteBuffers, 1, &id };
+                     VertexBufferDynamic::UnBind();
+                   } }
 {
-  GLCall{ glGenBuffers, 1, &m_renderer_id };
-  GLCall{ glBindBuffer, GL_ARRAY_BUFFER, m_renderer_id };
-  GLCall{ glBufferData,
-          GL_ARRAY_BUFFER,
-          static_cast<std::ptrdiff_t>(count * sizeof(Quad)),
-          nullptr,
-          GL_DYNAMIC_DRAW };
-}
-
-void
-  swap(
-    VertexBufferDynamic &first,
-    VertexBufferDynamic &second) noexcept// nothrow
-{
-  // enable ADL (not necessary in our case, but good practice)
-  using std::swap;
-
-  // by swapping the members of two objects,
-  // the two objects are effectively swapped
-  swap(first.m_renderer_id, second.m_renderer_id);
-  swap(first.m_max_size, second.m_max_size);
 }
--- src/imgui/opengl/episode_21/VertexBufferDynamic.hpp
+++ src/imgui/opengl/episode_21/VertexBufferDynamic.hpp
@@ -4,58 +4,22 @@
 
 #ifndef MYPROJECT_VertexBufferDynamic_HPP
 #define MYPROJECT_VertexBufferDynamic_HPP
+#include "IndexBufferDynamicSize.hpp"
 #include "Renderer.hpp"
+#include "unique_value.hpp"
 #include "Vertex.hpp"
 #include <cstdint>
 #include <ranges>
-class IndexBufferDynamicSize
-{
-private:
-  std::size_t m_size{};
 
-public:
-  constexpr IndexBufferDynamicSize() noexcept = default;
-  constexpr IndexBufferDynamicSize(std::size_t in_size) noexcept
-    : m_size(in_size)
-  {
-  }
-  constexpr std::size_t
-    size() const noexcept
-  {
-    return m_size;
-  }
-  constexpr void
-    Bind() const noexcept
-  {
-  }
-
-  static constexpr void
-    UnBind() noexcept
-  {
-  }
-};
-static_assert(
-  SizedBindable<
-    IndexBufferDynamicSize> && std::copyable<IndexBufferDynamicSize>);
 class VertexBufferDynamic
 {
 private:
-  std::uint32_t m_renderer_id = {};
-  std::size_t   m_max_size{};
+  GLID        m_renderer_id = {};
+  std::size_t m_max_size{};
 
 public:
   VertexBufferDynamic() = default;
   VertexBufferDynamic(size_t count);
-  ~VertexBufferDynamic();
-
-  VertexBufferDynamic(const VertexBufferDynamic &) = delete;
-  VertexBufferDynamic &
-    operator=(const VertexBufferDynamic &) = delete;
-
-  VertexBufferDynamic(VertexBufferDynamic &&other) noexcept;
-  VertexBufferDynamic &
-    operator=(VertexBufferDynamic &&other) noexcept;
-
   void
     Bind() const;
   static void
@@ -71,15 +35,11 @@ public:
             0,
             static_cast<std::ptrdiff_t>(
               std::ranges::size(vertices)
-              * sizeof(std::ranges::range_value_t<decltype(vertices)>)),
+              * sizeof(std::ranges::range_value_t<T>)),
             std::ranges::data(vertices) };
     return IndexBufferDynamicSize(
       (std::ranges::size(vertices) / std::size(Quad{}) * 6U));
   }
-
-
-  friend void
-    swap(VertexBufferDynamic &first, VertexBufferDynamic &second) noexcept;
 };
 static_assert(Bindable<VertexBufferDynamic>);
 
