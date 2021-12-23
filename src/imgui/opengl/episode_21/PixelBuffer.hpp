fd2c2d5bdcf0fece27c37f33a13849ca8ae0d1da
Sebanisu <peeceseven@gmail.com>
2021-12-23 04:37:27 -0500

    add public add path storage. add const array_size
    add comments
    
    removed the push and pop function. so you have to either push or pop.
    
    though it's not really front or back. it rotates around the array from front to end forever.

--- src/imgui/opengl/episode_21/PixelBuffer.hpp
+++ src/imgui/opengl/episode_21/PixelBuffer.hpp
@@ -4,24 +4,24 @@
 
 #ifndef MYPROJECT_PIXELBUFFER_HPP
 #define MYPROJECT_PIXELBUFFER_HPP
-#include "../../../../cmake-build-debug-wsl/src/imgui/opengl/episode_21/CMakeFiles/opengl_series_episode_21.dir/Debug/cmake_pch.hxx"
 #include "FrameBuffer.hpp"
 #include "scope_guard.hpp"
 #include "unique_value.hpp"
 class PixelBuffer
 {
+  constexpr static inline std::size_t ARRAY_SIZE = { 2U };
   // wrapping this pointer to force it to call glUnmapBuffer. Should scope this.
   using unique_glubyte =
     std::unique_ptr<const uint8_t, decltype([](const GLubyte *const) {
                       glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
                     })>;
 
-
+public:
   PixelBuffer(const FrameBufferSpecification &fbs)
     : width(fbs.width)
     , height(fbs.height)
     , DATA_SIZE(fbs.height * fbs.width * 4)
-    , pbos{ [](GLID_array<2>::ParameterT ids) {
+    , pbos{ [](GLID_array<ARRAY_SIZE>::ParameterT ids) {
              glDeleteBuffers(
                static_cast<std::int32_t>(std::ranges::size(ids)),
                std::ranges::data(ids));
@@ -42,16 +42,34 @@ class PixelBuffer
             } }
   {
   }
+  /**
+   * Push front a frame buffer
+   * @param fb where the image data has been rendered
+   * @param path to be forwarded to call back for saving later.
+   * @param full pointer to bool if all values are present.
+   * @return true if any values are present
+   */
   bool operator()(
-    const FrameBuffer                    &fb,
-    std::invocable<std::span<const std::uint8_t>> auto &&call_back) const
+    const FrameBuffer    &fb,
+    std::filesystem::path path,
+    bool *const           full = nullptr) const
   {
     Next();
-    ToPBO(fb);
-    FromPBO(std::forward<decltype(call_back)>(call_back));
+    ToPBO(fb, std::move(path));
+    if (full != nullptr)
+    {
+      *full = std::ranges::all_of(set, std::identity{});
+    }
     return std::ranges::any_of(set, std::identity{});
   }
-  bool operator()(std::invocable<std::uint8_t *> auto &&call_back) const
+  /**
+   * pop back the last one and run call back
+   * @param call_back accepts image data and does something
+   * @return true if any values are still present
+   */
+  bool operator()(
+    std::invocable<std::span<const std::uint8_t>, std::filesystem::path> auto
+      &&call_back) const
   {
     Next();
     FromPBO(std::forward<decltype(call_back)>(call_back));
@@ -59,7 +77,9 @@ class PixelBuffer
   }
 
 private:
-  void FromPBO(std::invocable<std::span<const std::uint8_t>> auto &&call_back) const
+  void FromPBO(
+    std::invocable<std::span<const std::uint8_t>, std::filesystem::path> auto
+      &&call_back) const
   {
     // map the PBO to process its data by CPU
     const scope_guard unbind_pbo_buffer = (&UnBind);
@@ -70,11 +90,14 @@ private:
         glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY)) };
       if (ptr)
       {
-        std::invoke(call_back, std::span{ptr.get(),static_cast<std::size_t>(DATA_SIZE)});
+        std::invoke(
+          call_back,
+          std::span{ ptr.get(), static_cast<std::size_t>(DATA_SIZE) },
+          std::move(paths[next_index]));
       }
     }
   }
-  void ToPBO(const FrameBuffer &fb) const
+  void ToPBO(const FrameBuffer &fb, std::filesystem::path path) const
   {
     // set the target framebuffer to read
     fb.Bind();
@@ -86,7 +109,8 @@ private:
     {
       const scope_guard unbind_pbo_buffer = (&UnBind);
       Bind(index);
-      set[index] = true;
+      set[index]   = true;
+      paths[index] = std::move(path);
       glReadPixels(0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, 0);
     }
   }
@@ -106,12 +130,13 @@ private:
     // back to conventional pixel operation
     glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
   }
-  mutable std::size_t         index      = {};
-  mutable std::size_t         next_index = { index + 1 };
-  std::int32_t                width      = {};
-  std::int32_t                height     = {};
-  std::int32_t                DATA_SIZE  = {};// number of bytes in image.
-  mutable std::array<bool, 2> set        = {};
-  GLID_array<2>               pbos       = {};
+  mutable std::size_t index      = {};
+  mutable std::size_t next_index = { (index + 1) % ARRAY_SIZE };
+  std::int32_t        width      = {};
+  std::int32_t        height     = {};
+  std::int32_t        DATA_SIZE  = {};// number of bytes in image.
+  mutable std::array<bool, ARRAY_SIZE>                  set   = {};
+  mutable std::array<std::filesystem::path, ARRAY_SIZE> paths = {};
+  GLID_array<ARRAY_SIZE>                                pbos  = {};
 };
 #endif// MYPROJECT_PIXELBUFFER_HPP
