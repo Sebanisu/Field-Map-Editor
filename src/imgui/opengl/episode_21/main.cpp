a2fb3ab117c5bc6148d583ebf2c491f718a6b53d
Sebanisu <peeceseven@gmail.com>
2022-01-03 10:27:43 -0500

    Update main.cpp

--- src/imgui/opengl/episode_21/main.cpp
+++ src/imgui/opengl/episode_21/main.cpp
@@ -2,8 +2,8 @@
 #include "Application.hpp"
 int main(void)
 {
-  //todo attach this scope gaurd to the api somewhere
-  // anything created in the window must be destroyed before the window.
+  // todo attach this scope gaurd to the api somewhere
+  //  anything created in the window must be destroyed before the window.
   const glengine::scope_guard_expensive end = { []() {
     ImGui::DestroyContext(nullptr);
     glfwTerminate();
