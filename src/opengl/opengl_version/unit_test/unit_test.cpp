5416a46be9f7c7884804d018601881790c4d1f65
Sebanisu <peeceseven@gmail.com>
2022-04-19 19:26:01 -0400

    add generic test.

--- src/opengl/opengl_version/unit_test/unit_test.cpp
+++ src/opengl/opengl_version/unit_test/unit_test.cpp
@@ -1,12 +1,8 @@
-//
-// Created by pcvii on 3/7/2022.
-//
-#ifndef BOOST_UT_DISABLE_MODULE
-#define BOOST_UT_DISABLE_MODULE
-#endif
 #include <boost/ut.hpp>
 int main()
 {
   using namespace boost::ut;
-
+  "boo"_test = []()
+  {
+  };
 }
