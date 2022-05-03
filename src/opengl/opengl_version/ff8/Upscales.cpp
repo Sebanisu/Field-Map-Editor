0fc4af2f6c70ae467620fde57fb4979893b1f653
Sebanisu <peeceseven@gmail.com>
2022-05-03 09:27:46 -0400

    added code to detect the file count in upscale folders.

--- src/opengl/opengl_version/ff8/Upscales.cpp
+++ src/opengl/opengl_version/ff8/Upscales.cpp
@@ -20,11 +20,13 @@ const std::string &ff8::Upscales::Path() const
 ff8::Upscales::Upscales()
 {
   using namespace std::literals::string_literals;
-  m_paths = std::vector{
-    tl::string::replace_slashes(R"(D:\Angelwing-Ultima_Remastered_v1-0-a)"s),
-    tl::string::replace_slashes(R"(/mnt/D/Angelwing-Ultima_Remastered_v1-0-a)"s)
+  m_paths =
+    std::vector{ tl::string::replace_slashes(
+                   R"(D:\Angelwing-Ultima_Remastered_v1-0-a\field_bg)"s),
+                 tl::string::replace_slashes(
+                   R"(/mnt/D/Angelwing-Ultima_Remastered_v1-0-a/field_bg)"s)
 
-  };
+    };
   const auto [first, last] =
     std::ranges::remove_if(m_paths, [](const std::filesystem::path path) {
       std::error_code ec{};
@@ -47,4 +49,61 @@ ff8::Upscales::Upscales()
       return !found || !is_dir;
     });
   m_paths.erase(first, last);
+  if (!std::ranges::empty(m_paths))
+  {
+    const auto     &path = m_paths.front();
+    std::error_code ec{};
+    const auto      handle_error = [&ec](int line, const auto &extra) {
+      if (ec)
+      {
+        fmt::print(
+          stderr,
+          "error {}:{} - {}: {}, - \"{}\"\n",
+          __FILE__,
+          line,
+          ec.value(),
+          ec.message(),
+          extra);
+      }
+    };
+    auto recursive_dir =
+      std::filesystem::recursive_directory_iterator(path, ec);
+    if (ec)
+    {
+      handle_error(__LINE__, path);
+      return;
+    }
+    using count_t =
+      std::iter_difference_t<decltype(std::filesystem::directory_iterator(
+        path, ec))>;
+    count_t max_count = {};
+    for (const auto &sub_path : recursive_dir)
+    {
+      const bool is_dir = std::filesystem::is_directory(sub_path, ec);
+      handle_error(__LINE__, path);
+      if (is_dir)
+      {
+        auto counting_path = std::filesystem::directory_iterator(sub_path, ec);
+        if (ec)
+        {
+          handle_error(__LINE__, sub_path.path().string());
+          continue;
+        }
+        const auto count = std::ranges::count_if(
+          counting_path, [&handle_error, &ec](const auto &item) {
+            bool is_regular_file = std::filesystem::is_regular_file(item, ec);
+            handle_error(__LINE__, item.path().string());
+            return is_regular_file;
+          });
+        if (count > 0)
+        {
+          fmt::print(
+            "path: \"{}\",\tfile count: {}\n", sub_path.path().string(), count);
+          if (max_count < count)
+            max_count = count;
+        }
+      }
+    }
+    fmt::print("max file count is: {}", max_count);
+  }
 }
