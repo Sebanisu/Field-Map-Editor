bd6d1784ed6c2da24f3225b1b06572b6ab74e9cc
Sebanisu <peeceseven@gmail.com>
2022-06-30 09:33:16 -0400

    some of this code is not compatible with vs2019 commented out unused code.

--- src/opengl/opengl_version/ff8/MapHistory.hpp
+++ src/opengl/opengl_version/ff8/MapHistory.hpp
@@ -7,32 +7,32 @@
 #include <ScopeGuard.hpp>
 namespace ff_8
 {
-template<typename TileT>
-struct [[nodiscard]] PairOfTiles
-{
-  PairOfTiles() = default;
-  PairOfTiles(const TileT &front, TileT &back)
-    : m_front_tile(&front)
-    , m_back_tile(&back)
-  {
-  }
-  [[nodiscard]] const TileT &front_tile() const noexcept
-  {
-    return *m_front_tile;
-  }
-  [[nodiscard]] TileT &back_tile() noexcept
-  {
-    return *m_back_tile;
-  }
-  [[nodiscard]] const TileT &back_tile() const noexcept
-  {
-    return *m_back_tile;
-  }
-
-private:
-  const TileT *m_front_tile;
-  TileT       *m_back_tile;
-};
+// template<typename TileT>
+// struct [[nodiscard]] PairOfTiles
+//{
+//   PairOfTiles() = default;
+//   PairOfTiles(const TileT &front, TileT &back)
+//     : m_front_tile(&front)
+//     , m_back_tile(&back)
+//   {
+//   }
+//   [[nodiscard]] const TileT &front_tile() const noexcept
+//   {
+//     return *m_front_tile;
+//   }
+//   [[nodiscard]] TileT &back_tile() noexcept
+//   {
+//     return *m_back_tile;
+//   }
+//   [[nodiscard]] const TileT &back_tile() const noexcept
+//   {
+//     return *m_back_tile;
+//   }
+//
+// private:
+//   const TileT *m_front_tile;
+//   TileT       *m_back_tile;
+// };
 class [[nodiscard]] MapHistory
 {
   enum class Pushed : std::uint8_t
@@ -76,24 +76,9 @@ class [[nodiscard]] MapHistory
     });
   }
 
-public:
-  MapHistory() = default;
-  MapHistory(MapT map)
-  {
-    m_maps.emplace_back(std::move(map));
-    m_maps.push_back(m_maps.front());
-  }
-  [[nodiscard]] std::size_t count() const
-  {
-    return m_maps.size() - 2U;
-  }
   [[nodiscard]] const MapT &front() const
   {
     return m_maps.front();
-  }
-  [[nodiscard]] MapT &back() const
-  {
-    return m_maps.back();
   }
   template<typename TileT>
   [[nodiscard]] auto get_offset_from_back(const TileT &tile) const
@@ -113,7 +98,6 @@ public:
       }
     });
   }
-
   template<typename TileT, std::integral PosT, typename LambdaT>
   [[nodiscard]] auto
     front_get_tile_at_offset(const PosT pos, LambdaT &&lambda) const
@@ -169,6 +153,22 @@ public:
         }
       }
     });
+  }
+
+public:
+  MapHistory() = default;
+  MapHistory(MapT map)
+  {
+    m_maps.emplace_back(std::move(map));
+    m_maps.push_back(m_maps.front());
+  }
+  [[nodiscard]] std::size_t count() const
+  {
+    return m_maps.size() - 2U;
+  }
+  [[nodiscard]] MapT &back() const
+  {
+    return m_maps.back();
   }
   template<typename TileT, typename LambdaT>
   auto copy_back_and_get_new_tile(const TileT &tile, LambdaT &&lambda) const
@@ -235,20 +235,20 @@ public:
     m_front_or_back.push_back(Pushed::Back);
     return m_maps.emplace_back(back());
   }
-  [[nodiscard]] const MapT &copy_back_to_front() const
-  {
-    const auto count = debug_count_print();
-    m_maps.insert(m_maps.begin(), back());
-    m_front_or_back.push_back(Pushed::Front);
-    return front();
-  }
-  [[nodiscard]] const MapT &copy_front() const
-  {
-    const auto count = debug_count_print();
-    m_maps.insert(m_maps.begin(), front());
-    m_front_or_back.push_back(Pushed::Front);
-    return front();
-  }
+  //  [[nodiscard]] const MapT &copy_back_to_front() const
+  //  {
+  //    const auto count = debug_count_print();
+  //    m_maps.insert(m_maps.begin(), back());
+  //    m_front_or_back.push_back(Pushed::Front);
+  //    return front();
+  //  }
+  //  [[nodiscard]] const MapT &copy_front() const
+  //  {
+  //    const auto count = debug_count_print();
+  //    m_maps.insert(m_maps.begin(), front());
+  //    m_front_or_back.push_back(Pushed::Front);
+  //    return front();
+  //  }
   /**
    * Deletes the most recent back or front
    * @return
@@ -284,60 +284,61 @@ public:
   {
     return m_maps.size() > 2U;
   }
-  [[nodiscard]] auto visit_both(auto &&function) const
-  {
-    return visit_both(function, std::identity{});
-  }
-  [[nodiscard]] auto visit_both(auto &&function, auto &&filter) const
-  {
-    return front().visit_tiles(
-      [this, &function, &filter](
-        const std::ranges::contiguous_range auto &front_tiles) {
-        back().visit_tiles([&front_tiles, &function, &filter](
-                             std::ranges::contiguous_range auto &&back_tiles) {
-          using TileT  = std::ranges::range_value_t<decltype(front_tiles)>;
-          using BTileT = std::ranges::range_value_t<decltype(back_tiles)>;
-          if constexpr (!std::is_same_v<TileT, BTileT>)
-          {
-            std::vector<PairOfTiles<TileT>> temp_mux        = {};
-            auto                            temp_mux_filter = filter(temp_mux);
-            return function(temp_mux_filter);
-          }
-          else
-          {
-            std::vector<PairOfTiles<TileT>> temp_mux = {};
-            temp_mux.reserve(std::ranges::size(front_tiles));
-            std::ranges::transform(
-              front_tiles,
-              back_tiles,
-              std::back_inserter(temp_mux),
-              [](const auto &front_tile, auto &back_tile) {
-                return PairOfTiles<TileT>(front_tile, back_tile);
-              });
-            auto temp_mux_filter = filter(temp_mux);
-            return function(temp_mux_filter);
-          }
-        });
-      });
-  }
-  [[nodiscard]] bool visit_both_tiles(
-    std::invocable auto &&function,
-    std::invocable auto &&filter = std::identity{})
-  {
-    return visit_both(
-      [&function](std::ranges::contiguous_range auto &&mux_tiles) -> bool {
-        bool changed = false;
-        for (auto &pair : mux_tiles)
-        {
-          if (function(std::as_const(pair.front_tile), pair.back_tile))
-          {
-            changed = true;
-          }
-        }
-        return changed;
-      },
-      filter);
-  }
+  //  [[nodiscard]] auto visit_both(auto &&function) const
+  //  {
+  //    return visit_both(function, std::identity{});
+  //  }
+  //  [[nodiscard]] auto visit_both(auto &&function, auto &&filter) const
+  //  {
+  //    return front().visit_tiles(
+  //      [this, &function, &filter](
+  //        const std::ranges::contiguous_range auto &front_tiles) {
+  //        back().visit_tiles([&front_tiles, &function, &filter](
+  //                             std::ranges::contiguous_range auto
+  //                             &&back_tiles) {
+  //          using TileT  = std::ranges::range_value_t<decltype(front_tiles)>;
+  //          using BTileT = std::ranges::range_value_t<decltype(back_tiles)>;
+  //          if constexpr (!std::is_same_v<TileT, BTileT>)
+  //          {
+  //            std::vector<PairOfTiles<TileT>> temp_mux        = {};
+  //            auto                            temp_mux_filter =
+  //            filter(temp_mux); return function(temp_mux_filter);
+  //          }
+  //          else
+  //          {
+  //            std::vector<PairOfTiles<TileT>> temp_mux = {};
+  //            temp_mux.reserve(std::ranges::size(front_tiles));
+  //            std::ranges::transform(
+  //              front_tiles,
+  //              back_tiles,
+  //              std::back_inserter(temp_mux),
+  //              [](const auto &front_tile, auto &back_tile) {
+  //                return PairOfTiles<TileT>(front_tile, back_tile);
+  //              });
+  //            auto temp_mux_filter = filter(temp_mux);
+  //            return function(temp_mux_filter);
+  //          }
+  //        });
+  //      });
+  //  }
+  //  [[nodiscard]] bool visit_both_tiles(
+  //    std::invocable auto &&function,
+  //    std::invocable auto &&filter = std::identity{})
+  //  {
+  //    return visit_both(
+  //      [&function](std::ranges::contiguous_range auto &&mux_tiles) -> bool {
+  //        bool changed = false;
+  //        for (auto &pair : mux_tiles)
+  //        {
+  //          if (function(std::as_const(pair.front_tile), pair.back_tile))
+  //          {
+  //            changed = true;
+  //          }
+  //        }
+  //        return changed;
+  //      },
+  //      filter);
+  //  }
 };
 }// namespace ff_8
 #endif// FIELD_MAP_EDITOR_MAPHISTORY_HPP
