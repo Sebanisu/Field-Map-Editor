d2fb83ddbe4c18c0b3adcbf4e0bffd4f8af38f7b
Sebanisu <peeceseven@gmail.com>
2022-09-22 13:51:09 -0400

    glm suppress warning C4201: nonstandard extension used: nameless struct/union in msvc

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -30,6 +30,7 @@ function(set_project_warnings project_name)
             /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
             /permissive- # standards conformance mode for MSVC compiler.
             /wd4068 # suppress unknown pragma. cmake is using #pragma GCC in precompiled header. MSVC no like.
+            /wd4201 # suppress nonstandard extension used : nameless struct/union
             )
 
     set(CLANG_WARNINGS
