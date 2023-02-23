ce17b6b82b4b5474bf27b2899e2c005f6a1d8e7c
Sebanisu <peeceseven@gmail.com>
2023-02-23 13:11:50 -0500

    Update Conan.cmake

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -6,9 +6,11 @@ macro(run_conan)
     if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
         file(
-                DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.17.0/conan.cmake"
+#                DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.17.0/conan.cmake"
+                DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.18/conan.cmake"
                 "${CMAKE_BINARY_DIR}/conan.cmake"
-                EXPECTED_HASH SHA256=3bef79da16c2e031dc429e1dac87a08b9226418b300ce004cc125a82687baeef
+#                EXPECTED_HASH SHA256=3bef79da16c2e031dc429e1dac87a08b9226418b300ce004cc125a82687baeef
+                EXPECTED_HASH SHA256=50bd7c9c076198334fb8365b009e516f97b3c9cb0fac3cf9637d12b88bc1a67e
                 TLS_VERIFY ON)
     endif ()
 
