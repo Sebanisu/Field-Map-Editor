b28e977dd20495acb8d3cd1ee8c5efcd9d8f0114
Sebanisu <peeceseven@gmail.com>
2023-02-23 13:50:27 -0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -14,13 +14,14 @@ class FieldMapEditor(ConanFile):
         "glfw/3.3.5",  # opengl version opengl library.
         "glew/2.2.0",  # opengl version opengl extension library
         "glm/0.9.9.8",  # opengl version opengl math library
-        "imgui/cci.20220207+1.87.docking", #docking UI for opengl
+        "imgui/cci.20230105+1.89.2.docking", #docking UI for opengl
         #"imgui/1.87",  # UI used for both opengl and sfml
         "sfml/2.5.1@#7971039289b1207a61814784bd619d01",  # sfml version simplified c++98 gaming library
         #"libunwind/1.6.2", # umm test program is looking for this?
         "zlib/1.2.13", # resolve conflict by using it here.
         "andreasbuhr-cppcoro/cci.20210113",  # sfml version uses coroutines. Thought this would help with async.
-        "stb/cci.20210713",  # image loading and saving
+        "stb/cci.20220909",  # image loading and saving
+        "openal-soft/1.22.2", # open audio library used by sfml.
         "boost-ext-ut/1.1.9",  # unit testing library / wip
         #"tomlplusplus/3.1.0"  #toml++  c++17 configuration library
         "tomlplusplus/3.0.1"
