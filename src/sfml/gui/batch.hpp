51fef49a1ba485089216b27d7e081b9a4e2b5563
Sebanisu <peeceseven@gmail.com>
2023-03-07 14:21:45 -0500

    testing textbox code

--- src/sfml/gui/batch.hpp
+++ src/sfml/gui/batch.hpp
@@ -9,6 +9,7 @@
 #include "filter.hpp"
 #include "generic_combo.hpp"
 #include "gui_labels.hpp"
+#include "safedir.hpp"
 #include <array>
 #include <imgui.h>
 class batch
@@ -17,11 +18,44 @@ class batch
      output_types                   m_output_type;
      ff_8::filter_old<compact_type> m_compact_type;
      ff_8::filter_old<flatten_type> m_flatten_type;
+     std::array<char, 256U>         m_input_path;
+     bool                           m_input_path_valid = false;
 
      void                           combo_input_type(int &imgui_id);
      void                           combo_output_type(int &imgui_id);
      void                           combo_compact_type(int &imgui_id);
      void                           combo_flatten_type(int &imgui_id);
+     void                           browse_input_path(int &imgui_id)
+     {
+          const auto pop_id = scope_guard{ &ImGui::PopID };
+          ImGui::PushID(++imgui_id);
+          // ImGui text box with browse button
+          // Highlight text box red if the folder doesn't exist
+          {
+               if (!m_input_path_valid)
+               {
+                    ImGui::PushStyleColor(ImGuiCol_FrameBg, (ImVec4)ImColor::HSV(0.0F, 0.6F, 0.6F));
+               }
+               const auto pop_color = scope_guard([valid = m_input_path_valid]() {
+                    if (!valid)
+                    {
+                         ImGui::PopStyleColor();
+                    }
+               });
+               if (ImGui::InputText("Folder Path", m_input_path.data(), m_input_path.size()))
+               {
+                    // Check if the folder path exists when the text box changes
+                    const auto tmp     = safedir(m_input_path.data());
+                    m_input_path_valid = tmp.is_exists() && tmp.is_dir();
+               }
+          }
+          ImGui::SameLine();
+          if (ImGui::Button("Browse"))
+          {
+               // Trigger the chooseFolder function when the button is clicked
+               // chooseFolder();
+          }
+     }
 
    public:
      void draw_window(int &imgui_id)
@@ -32,6 +66,7 @@ class batch
                combo_output_type(imgui_id);
                combo_compact_type(imgui_id);
                combo_flatten_type(imgui_id);
+               browse_input_path(imgui_id);
           }
           ImGui::End();
      }
