9cdc87810d4bd55fb17c2b56007d9ffa4c6cf317
Sebanisu <peeceseven@gmail.com>
2025-02-06 14:36:27 -0500

    add keys

--- src/sfml/gui/custom_paths_window.hpp
+++ src/sfml/gui/custom_paths_window.hpp
@@ -13,6 +13,7 @@
 #include <open_viii/strings/LangCommon.hpp>
 #include <optional>
 #include <ranges>
+#include <string_view>
 #include <tl/string.hpp>
 namespace fme
 {
@@ -25,40 +26,74 @@ struct custom_paths_map
      std::optional<std::uint8_t>     texture_page  = {};
      std::optional<std::uint32_t>    pupu_id       = {};
 
-     std::string                     operator()(std::string_view key, const std::shared_ptr<Selections> &selections) const
+     struct keys
      {
-          using namespace std::string_view_literals;
+          static constexpr std::string_view ff8_path              = { "ff8_path" };
+          static constexpr std::string_view ffnx_mod_path         = { "ffnx_mod_path" };
+          static constexpr std::string_view ffnx_direct_mode_path = { "ffnx_direct_mode_path" };
+          static constexpr std::string_view ffnx_override_path    = { "ffnx_override_path" };
+          static constexpr std::string_view input_path            = { "input_path" };
+          static constexpr std::string_view batch_input_path      = { "batch_input_path" };
+          static constexpr std::string_view batch_output_path     = { "batch_output_path" };
+          static constexpr std::string_view field_name            = { "field_name" };
+          static constexpr std::string_view ext                   = { "ext" };
+          static constexpr std::string_view field_prefix          = { "field_prefix" };
+          static constexpr std::string_view letter_2_lang         = { "2_letter_lang" };
+          static constexpr std::string_view letter_3_lang         = { "3_letter_lang" };
+          static constexpr std::string_view palette               = { "palette" };
+          static constexpr std::string_view texture_page          = { "texture_page" };
+          static constexpr std::string_view pupu_id               = { "pupu_id" };
+          static constexpr std::string_view demaster_mod_path     = { "demaster_mod_path" };
+          static constexpr std::string_view ffnx_multi_texture    = { "ffnx_multi_texture" };
+          static constexpr std::string_view ffnx_single_texture   = { "ffnx_single_texture" };
+          static constexpr std::string_view ffnx_map              = { "ffnx_map" };
+          static constexpr std::string_view demaster              = { "demaster" };
+
+          static constexpr auto             all_keys              = std::to_array<std::string_view>({ ff8_path,
+                                                                                                      ffnx_mod_path,
+                                                                                                      ffnx_direct_mode_path,
+                                                                                                      ffnx_override_path,
+                                                                                                      input_path,
+                                                                                                      batch_input_path,
+                                                                                                      batch_output_path,
+                                                                                                      field_name,
+                                                                                                      ext,
+                                                                                                      field_prefix,
+                                                                                                      letter_2_lang,
+                                                                                                      letter_3_lang,
+                                                                                                      palette,
+                                                                                                      texture_page,
+                                                                                                      pupu_id,
+                                                                                                      demaster_mod_path,
+                                                                                                      ffnx_multi_texture,
+                                                                                                      ffnx_single_texture,
+                                                                                                      ffnx_map,
+                                                                                                      demaster });
+     };
+
+     std::string operator()(std::string_view key, const std::shared_ptr<Selections> &selections) const
+     {
           if (selections)
           {
-               if ("ff8_path"sv == key)
+               if (keys::ff8_path == key)
                {
                     return selections->path;
                }
-               else if ("ffnx_mod_path"sv == key)
+               else if (keys::ffnx_mod_path == key)
                {
                     return selections->ffnx_mod_path;
                }
-               else if ("ffnx_direct_mode_path"sv == key)
+               else if (keys::ffnx_direct_mode_path == key)
                {
                     return selections->ffnx_direct_mode_path;
                }
-               else if ("ffnx_override_path"sv == key)
+               else if (keys::ffnx_override_path == key)
                {
                     return selections->ffnx_override_path;
                }
-               else if ("input_path"sv == key)
+               else if (keys::input_path == key)
                {
                     return selections->path;
-               }
-               else if ("batch_input_path"sv == key)
-               {
-                    const Configuration config{};
-                    return config["batch_input_path"].value_or(std::string{});
-               }
-               else if ("batch_output_path"sv == key)
-               {
-                    const Configuration config {};
-                    return config["batch_output_path"].value_or(std::string{});
                }
           }
           return operator()(key);
@@ -66,53 +101,88 @@ struct custom_paths_map
 
      std::string operator()(std::string_view key) const
      {
-          using namespace std::string_view_literals;
-          if ("field_name"sv == key)
+          if (keys::field_name == key)
           {
                assert(!field_name.empty() && field_name.length() >= 3);
                return fmt::format("{}", field_name);
           }
-          else if ("ext"sv == key)
+          else if (keys::ext == key)
           {
                assert(ext.size() >= 2 && ext[0] == '.');
                return fmt::format("{}", ext);
           }
-          else if ("field_prefix"sv == key)
+          else if (keys::field_prefix == key)
           {
                return fmt::format("{}", field_prefix());
           }
-          else if ("2_letter_lang"sv == key)
+          else if (keys::letter_2_lang == key)
           {
                return fmt::format("{}", letter_2_lang());
           }
-          else if ("3_letter_lang"sv == key)
+          else if (keys::letter_3_lang == key)
           {
                return fmt::format("{}", letter_3_lang());
           }
-          else if ("palette"sv == key)
+          else if (keys::palette == key)
           {
                if (palette.has_value())
                {
                     return fmt::format("{:02}", palette.value());
                }
           }
-          else if ("texture_page"sv == key)
+          else if (keys::texture_page == key)
           {
                if (texture_page.has_value())
                {
                     return fmt::format("{:02}", texture_page.value());
                }
           }
-          else if ("pupu_id"sv == key)
+          else if (keys::pupu_id == key)
           {
                if (pupu_id.has_value())
                {
                     return fmt::format("{:08x}", pupu_id.value());
                }
           }
-          else if ("demaster_mod_path"sv == key)
+          else if (keys::demaster_mod_path == key)
           {
-               return "DEMASTER_EXP";
+               const static auto path = std::string("DEMASTER_EXP");
+               return path;
+          }
+          else if (keys::batch_input_path == key)
+          {
+               const Configuration config{};
+               return config["batch_input_path"].value_or(std::string{});
+          }
+          else if (keys::batch_output_path == key)
+          {
+               const Configuration config{};
+               return config["batch_output_path"].value_or(std::string{});
+          }
+          else if (keys::ffnx_multi_texture == key)
+          {
+               const static auto path = std::string(
+                 "{ff8_path}/{ffnx_mod_path}/field/mapdata/{field_prefix}/{field_name}/{field_name}{_{2_letter_lang}}_{texture_page}{ext}");
+               return path;
+          }
+          else if (keys::ffnx_single_texture == key)
+          {
+               const static auto path =
+                 std::string("{ff8_path}/{ffnx_mod_path}/field/mapdata/{field_name}/{field_name}{_{2_letter_lang}}{ext}");
+               return path;
+          }
+          else if (keys::ffnx_map == key)
+          {
+               const static auto path = std::string(
+                 "{ff8_path}/{ffnx_direct_mode_path}/field/mapdata/{field_prefix}/{field_name}/{field_name}{_{2_letter_lang}}{ext}");
+               return path;
+          }
+          else if (keys::demaster == key)
+          {
+               const static auto path = std::string(
+                 "{ff8_path}/{demaster_mod_path}/textures/field_bg/{field_prefix}/{field_name}/"
+                 "{field_name}{_{2_letter_lang}}_{texture_page}{_{palette}}{ext}");
+               return path;
           }
           return {};
      }
@@ -191,20 +261,29 @@ struct custom_paths_window
                output_tests.clear();
                for (const auto test_data : tests)
                {
-                    auto &output_test = output_tests.emplace_back(input_path_str.begin(), input_path_str.end());
-                    for ([[maybe_unused]] const auto &match :
-                         ctre::search_all<R"(\{([^\{\}]+)\}|\{([^\{]*)\{([^\}]+)\}([^\}]*)\})">(input_path_str))
+                    auto                 &output_test = output_tests.emplace_back(input_path_str.begin(), input_path_str.end());
+                    constexpr static auto pattern     = CTRE_REGEX_INPUT_TYPE{ R"(\{([^\{\}]+)\}|\{([^\{]*)\{([^\}]+)\}([^\}]*)\})" };
+                    auto                  input_test  = output_test;
+                    auto                  matches     = ctre::search_all<pattern>(input_test);
+                    do
                     {
-                         const auto replace_str = std::string_view{ match.get<0>() };
-                         const auto key         = std::string_view{ match.get<1>() }.empty() ? std::string_view{ match.get<3>() }
-                                                                                             : std::string_view{ match.get<1>() };
-                         const auto value       = test_data(key, selections);
-                         const auto prefix      = value.empty() ? std::string_view{} : std::string_view{ match.get<2>() };
-                         const auto suffix      = value.empty() ? std::string_view{} : std::string_view{ match.get<4>() };
-                         output_test            = output_test | std::views::split(replace_str)
-                                       | std::views::join_with(fmt::format("{}{}{}", prefix, value, suffix))
-                                       | std::ranges::to<std::string>();
-                    }                    
+
+                         for ([[maybe_unused]] const auto &match : matches)
+                         {
+                              const auto replace_str = std::string_view{ match.get<0>() };
+                              const auto key         = std::string_view{ match.get<1>() }.empty() ? std::string_view{ match.get<3>() }
+                                                                                                  : std::string_view{ match.get<1>() };
+                              const auto value       = test_data(key, selections);
+                              const auto prefix      = value.empty() ? std::string_view{} : std::string_view{ match.get<2>() };
+                              const auto suffix      = value.empty() ? std::string_view{} : std::string_view{ match.get<4>() };
+                              output_test            = output_test | std::views::split(replace_str)
+                                            | std::views::join_with(fmt::format("{}{}{}", prefix, value, suffix))
+                                            | std::ranges::to<std::string>();
+                         }
+
+                         input_test = output_test;
+                         matches    = ctre::search_all<pattern>(input_test);
+                    } while (!std::ranges::empty(matches));
                     tl::string::replace_slashes(output_test);
                }
           }
