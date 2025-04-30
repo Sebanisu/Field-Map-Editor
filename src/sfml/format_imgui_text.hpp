b1257e5cb5cdb02a4657d165b33c45256bb13707
Sebanisu <peeceseven@gmail.com>
2025-04-30 17:01:53 -0400

    fix wrapped text so it uses as_const.
    I guess it doesn't like getting non const temporary references. even though it's just making a string and going poof anyway.

--- src/sfml/format_imgui_text.hpp
+++ src/sfml/format_imgui_text.hpp
@@ -23,7 +23,10 @@ inline const auto format_imgui_text = []<std::size_t sizeT = 0x1FEU, typename...
 inline const auto format_imgui_wrapped_text = []<std::size_t sizeT = 0x1FEU, typename... T>(fmt::format_string<T...> fmt, T &&...items) {
      std::array<char, sizeT> buffer{};
      const auto              it = fmt::vformat_to_n(
-       std::begin(buffer), sizeT - 1U, fmt::string_view(fmt), fmt::make_format_args(std::forward<decltype(items)>(items)...));
+       std::begin(buffer),
+       sizeT - 1U,
+       fmt::string_view(fmt),
+       fmt::make_format_args(std::forward<decltype(std::as_const(items))>(std::as_const(items))...));
      *it.out = '\0';
      ImGui::TextWrapped("%s", std::data(buffer));
      // it seems ImGui::Text is coping the values into it's own buffer.
