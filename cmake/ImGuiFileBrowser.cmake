00ea3dab9eccb97dd21fa6d3cf3cf45426379b61
Sebanisu <peeceseven@gmail.com>
2025-05-21 10:19:49 -0400

    this seems to work

--- cmake/ImGuiFileBrowser.cmake
+++ cmake/ImGuiFileBrowser.cmake
@@ -1,17 +1,14 @@
-include(FetchContent)
+
 #options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
+include(FetchContent)
 FetchContent_Declare(
-        imgui_filebrowser_fetch
-        #GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
-        GIT_REPOSITORY https://github.com/sebanisu/imgui-filebrowser.git
-        GIT_TAG origin/master
-        GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+    imgui_filebrowser_fetch
+    #GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
+    GIT_REPOSITORY https://github.com/sebanisu/imgui-filebrowser.git
+    GIT_TAG origin/master
+    GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
 )
-# include imfilebrowser.h
-#FetchContent_MakeAvailable(imgui_filebrowser_fetch)
-FetchContent_GetProperties(imgui_filebrowser_fetch)
-if (NOT imgui_filebrowser_fetch_POPULATED)
-    FetchContent_Populate(imgui_filebrowser_fetch)
-    add_library(imgui_filebrowser INTERFACE)
-    target_include_directories(imgui_filebrowser INTERFACE ${imgui_filebrowser_fetch_SOURCE_DIR})
-endif ()
+FetchContent_MakeAvailable(imgui_filebrowser_fetch)
+
+add_library(imgui_filebrowser INTERFACE)
+target_include_directories(imgui_filebrowser INTERFACE ${imgui_filebrowser_fetch_SOURCE_DIR})
