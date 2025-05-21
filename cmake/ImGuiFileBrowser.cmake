a694b200e6a8d7f1986dd652581c17fc07d4dbea
Sebanisu <peeceseven@gmail.com>
2025-05-21 10:24:15 -0400

    I don't trust MakeAvailable

--- cmake/ImGuiFileBrowser.cmake
+++ cmake/ImGuiFileBrowser.cmake
@@ -1,14 +1,15 @@
-
-#options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
 include(FetchContent)
-FetchContent_Declare(
+# Declare the content without using FetchContent_Declare
+set(FETCHCONTENT_BASE_DIR "${CMAKE_BINARY_DIR}/_deps") # Optional: Customize where sources are stored
+FetchContent_Populate(
     imgui_filebrowser_fetch
+    QUIET
     #GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
     GIT_REPOSITORY https://github.com/sebanisu/imgui-filebrowser.git
     GIT_TAG origin/master
     GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_src"
+    BINARY_DIR "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_bin"
 )
-FetchContent_MakeAvailable(imgui_filebrowser_fetch)
-
 add_library(imgui_filebrowser INTERFACE)
-target_include_directories(imgui_filebrowser INTERFACE ${imgui_filebrowser_fetch_SOURCE_DIR})
+target_include_directories(imgui_filebrowser INTERFACE "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_src")
