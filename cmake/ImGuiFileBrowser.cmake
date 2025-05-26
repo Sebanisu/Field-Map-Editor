c1ca5387e4c97ee4e7b2bad9311666a87a76202c
Sebanisu <peeceseven@gmail.com>
2025-05-26 00:31:41 -0400

    Update ImGuiFileBrowser.cmake
    remove FetchContent_Populate

--- cmake/ImGuiFileBrowser.cmake
+++ cmake/ImGuiFileBrowser.cmake
@@ -1,17 +1,15 @@
 include(FetchContent)
-#options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
-FetchContent_Declare(
-        imgui_filebrowser_fetch
-        GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
-        #GIT_REPOSITORY https://github.com/sebanisu/imgui-filebrowser.git
-        GIT_TAG origin/master
-        GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+# Declare the content without using FetchContent_Declare
+set(FETCHCONTENT_BASE_DIR "${CMAKE_BINARY_DIR}/_deps") # Optional: Customize where sources are stored
+FetchContent_Populate(
+    imgui_filebrowser_fetch
+    QUIET
+    #GIT_REPOSITORY https://github.com/AirGuanZ/imgui-filebrowser.git
+    GIT_REPOSITORY https://github.com/sebanisu/imgui-filebrowser.git
+    GIT_TAG origin/master
+    GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_src"
+    BINARY_DIR "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_bin"
 )
-# include imfilebrowser.h
-#FetchContent_MakeAvailable(imgui_filebrowser_fetch)
-FetchContent_GetProperties(imgui_filebrowser_fetch)
-if (NOT imgui_filebrowser_fetch_POPULATED)
-    FetchContent_Populate(imgui_filebrowser_fetch)
-    add_library(imgui_filebrowser INTERFACE)
-    target_include_directories(imgui_filebrowser INTERFACE ${imgui_filebrowser_fetch_SOURCE_DIR})
-endif ()
+add_library(imgui_filebrowser INTERFACE)
+target_include_directories(imgui_filebrowser INTERFACE "${CMAKE_BINARY_DIR}/_deps/imgui_filebrowser_src")
