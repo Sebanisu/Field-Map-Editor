d171ebc99bfa2d07502c2ec364aac2884c0baf06
Sebanisu <peeceseven@gmail.com>
2025-06-04 20:24:33 -0400

    tweaks to sort_and_remove_duplicates

--- src/sfml/utilities.cpp
+++ src/sfml/utilities.cpp
@@ -4,5 +4,6 @@
 namespace fme
 {
 // Explicit instantiation for specific types
-template void sort_and_remove_duplicates(std::vector<std::string> &, std::vector<bool> &);
+template bool sort_and_remove_duplicates(std::vector<std::string> &, std::vector<bool> &);
+template bool sort_and_remove_duplicates(std::vector<std::string> &);
 }
--- src/sfml/utilities.hpp
+++ src/sfml/utilities.hpp
@@ -9,18 +9,27 @@ template<typename R>
 concept erasable_range = std::ranges::range<R> && requires(R &r) {
      { r.erase(std::ranges::begin(r), std::ranges::end(r)) } -> std::same_as<typename R::iterator>;
 };
-template<erasable_range R>
-inline void sort_and_remove_duplicates(R auto &...ranges)
+template<erasable_range... R>
+constexpr inline bool sort_and_remove_duplicates(R &...ranges) noexcept
 {
+     bool changed  = false;
+     const auto projection = [](const auto &values) { return std::get<0>(values); };
      auto zip_view = std::ranges::views::zip(ranges...);
+     if (!std::ranges::is_sorted(zip_view, {}, projection))
+     {
+          std::ranges::sort(zip_view, {}, projection);
+          changed = true;
+     }
+     if (std::ranges::adjacent_find(zip_view, {}, projection) != zip_view.end())
+     {
+          const auto unique_range        = std::ranges::unique(zip_view, {}, projection);
 
-     std::ranges::sort(zip_view, {}, [](const auto &values) { return std::get<0>(values); });
+          const auto unique_begin_offset = std::ranges::distance(zip_view.begin(), unique_range.begin());
 
-     const auto unique_range        = std::ranges::unique(zip_view, {}, [](const auto &values) { return std::get<0>(values); });
-
-     const auto unique_begin_offset = std::ranges::distance(zip_view.begin(), unique_range.begin());
-
-     (ranges.erase(std::ranges::next(ranges.begin(), unique_begin_offset), ranges.end()), ...);
+          (ranges.erase(std::ranges::next(ranges.begin(), unique_begin_offset), ranges.end()), ...);
+          changed = true;
+     }
+     return changed;
 }
 }// namespace fme
 
