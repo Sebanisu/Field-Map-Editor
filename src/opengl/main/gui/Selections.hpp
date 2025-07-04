16951543db080faefc23f3cbea004e305b67298c
Sebanisu <peeceseven@gmail.com>
2025-07-03 22:53:38 -0400

    Update Selections.hpp
    [-Werror=sign-conversion]

--- src/opengl/main/gui/Selections.hpp
+++ src/opengl/main/gui/Selections.hpp
@@ -274,7 +274,7 @@ struct Selections
       * Initializes to the current working directory on construction. If an error occurs while
       * determining the path, it logs a warning using spdlog.
       */
-     std::string                    path;
+     std::string                   path;
 
      /**
       * @brief The default window width.
@@ -286,63 +286,63 @@ struct Selections
       */
      static constexpr std::int32_t window_height_default = 720;
 
-     open_viii::graphics::BPPT      bpp;///< Bits per pixel for rendering.
-     uint8_t                        palette;///< Selected palette index.
+     open_viii::graphics::BPPT     bpp;///< Bits per pixel for rendering.
+     uint8_t                       palette;///< Selected palette index.
      // int                            field         = {};///< Selected field index. Set by gui after reading starter_field
-     std::string                    starter_field;///< field name.
-     open_viii::LangT               coo;///< Selected language.
-     draw_mode                      draw;///< Current drawing mode.
-     int                            selected_tile;///< Index of the currently selected tile.
-     int                            upscale_paths_index;
-     std::uint32_t                  window_width;///< Current window width.
-     std::uint32_t                  window_height;///< Current window height.
-     tile_sizes                     tile_size_value;///< Current tile size setting.
+     std::string                   starter_field;///< field name.
+     open_viii::LangT              coo;///< Selected language.
+     draw_mode                     draw;///< Current drawing mode.
+     int                           selected_tile;///< Index of the currently selected tile.
+     int                           upscale_paths_index;
+     std::int32_t                  window_width;///< Current window width.
+     std::int32_t                  window_height;///< Current window height.
+     tile_sizes                    tile_size_value;///< Current tile size setting.
 
-     bool                           draw_palette;///< Whether to draw the palette.
-     bool                           draw_grid;///< Whether to draw the grid.
-     bool                           draw_texture_page_grid;///< Whether to draw the texture page grid.
-     bool                           draw_tile_conflict_rects;///< Whether to draw the tile conflicts rectangles.
-     bool                           draw_swizzle;///< Whether to enable swizzle rendering.
-     bool                           draw_disable_blending;///< Whether to disable blending during rendering.
-     bool                           display_batch_window;///< Whether to display the test batch window.
+     bool                          draw_palette;///< Whether to draw the palette.
+     bool                          draw_grid;///< Whether to draw the grid.
+     bool                          draw_texture_page_grid;///< Whether to draw the texture page grid.
+     bool                          draw_tile_conflict_rects;///< Whether to draw the tile conflicts rectangles.
+     bool                          draw_swizzle;///< Whether to enable swizzle rendering.
+     bool                          draw_disable_blending;///< Whether to disable blending during rendering.
+     bool                          display_batch_window;///< Whether to display the test batch window.
      // todo fix imports
      //  bool                     display_import_image_window = false;
-     bool                           display_import_image;///< Whether to display the imported image.
-     bool                           force_rendering_of_map;///< Force rendering of map on every frame.
-     bool                           import_image_grid;///< Whether to overlay a grid on the imported image.
-     bool                           render_imported_image;///< Whether to render the imported image.
-     bool                           display_history_window;///< Whether to display history window.
-     bool                           display_control_panel_window;///< Whether to display control panel window.
-     bool                           display_draw_window;///< Whether to display draw window.
-     bool                           display_custom_paths_window;///< Whether to display custom paths window.
-     bool                           display_field_file_window;///< Whether to display field file window.
-     std::string                    ffnx_mod_path;
-     std::string                    ffnx_override_path;
-     std::string                    ffnx_direct_mode_path;
-     std::string                    output_swizzle_pattern;
-     std::string                    output_deswizzle_pattern;
-     std::string                    output_map_pattern_for_swizzle;
-     std::string                    output_map_pattern_for_deswizzle;
-     std::string                    swizzle_path;
-     std::string                    deswizzle_path;
-     std::string                    output_map_path;
-     std::string                    output_mim_path;
-     std::string                    output_image_path;
-     std::string                    import_load_image_directory;
-     PatternSelector                current_pattern;
-     int                            current_pattern_index;
-     color                          background_color;///< Remember to user's selected Background Color for draw Window.
+     bool                          display_import_image;///< Whether to display the imported image.
+     bool                          force_rendering_of_map;///< Force rendering of map on every frame.
+     bool                          import_image_grid;///< Whether to overlay a grid on the imported image.
+     bool                          render_imported_image;///< Whether to render the imported image.
+     bool                          display_history_window;///< Whether to display history window.
+     bool                          display_control_panel_window;///< Whether to display control panel window.
+     bool                          display_draw_window;///< Whether to display draw window.
+     bool                          display_custom_paths_window;///< Whether to display custom paths window.
+     bool                          display_field_file_window;///< Whether to display field file window.
+     std::string                   ffnx_mod_path;
+     std::string                   ffnx_override_path;
+     std::string                   ffnx_direct_mode_path;
+     std::string                   output_swizzle_pattern;
+     std::string                   output_deswizzle_pattern;
+     std::string                   output_map_pattern_for_swizzle;
+     std::string                   output_map_pattern_for_deswizzle;
+     std::string                   swizzle_path;
+     std::string                   deswizzle_path;
+     std::string                   output_map_path;
+     std::string                   output_mim_path;
+     std::string                   output_image_path;
+     std::string                   import_load_image_directory;
+     PatternSelector               current_pattern;
+     int                           current_pattern_index;
+     color                         background_color;///< Remember to user's selected Background Color for draw Window.
 
-     input_types                    batch_input_type;
-     root_path_types                batch_input_root_path_type;
-     output_types                   batch_output_type;
-     root_path_types                batch_output_root_path_type;
-     std::vector<bool>              batch_map_list_enabled;
+     input_types                   batch_input_type;
+     root_path_types               batch_input_root_path_type;
+     output_types                  batch_output_type;
+     root_path_types               batch_output_root_path_type;
+     std::vector<bool>             batch_map_list_enabled;
 
-     std::string                    batch_input_path;
-     std::string                    batch_output_path;
-     bool                           batch_input_load_map;
-     bool                           batch_output_save_map;
+     std::string                   batch_input_path;
+     std::string                   batch_output_path;
+     bool                          batch_input_load_map;
+     bool                          batch_output_save_map;
 
      ff_8::filter_old<compact_type, ff_8::FilterTag::Compact> batch_compact_type;
      ff_8::filter_old<flatten_type, ff_8::FilterTag::Flatten> batch_flatten_type;
