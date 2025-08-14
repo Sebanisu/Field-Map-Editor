9f3e5639e239a80cbe0b847349f22fe9f665a3f7
Sebanisu <peeceseven@gmail.com>
2025-08-13 22:39:29 -0400

    brain storm import process.

--- src/opengl/main/PupuID.cpp
+++ src/opengl/main/PupuID.cpp
@@ -50,3 +50,53 @@ namespace ff_8
 // IDs.end()); auto it = std::unique(IDs.begin(), IDs.end()); IDs.erase(it,
 // IDs.end());
 }// namespace ff_8
+
+
+// struct PupuIDComparator
+// {
+//      bool operator()(const PupuID &a, const PupuID &b) const
+//      {
+//           auto key = [](const PupuID &id) {
+//                return std::tuple{ // 1. prefer static images (animation_id == 255)
+//                                   id.animation_id() == 255 ? 0 : 1,
+
+//                                   // 2. prefer blend mode none
+//                                   id.blend_mode() == BlendMode::None ? 0 : 1,
+
+//                                   // 3. prefer animated if still tied
+//                                   id.animation_id() != 255 ? 0 : 1,
+
+//                                   // 4. if blend mode != none, group by blend mode
+//                                   id.blend_mode() != BlendMode::None ? static_cast<int>(id.blend_mode()) : -1,
+
+//                                   // 5. prefer smallest offset
+//                                   id.offset(),
+
+//                                   // 6. prefer grid aligned
+//                                   (id.is_x_not_aligned_to_grid() || id.is_y_not_aligned_to_grid()) ? 1 : 0
+//                };
+//           };
+
+//           return key(a) < key(b);
+//      }
+// };
+// struct PupuIDComparator
+// {
+//      const std::unordered_map<PupuID, size_t> *pupuCount;
+
+//      bool                                      operator()(const PupuID &a, const PupuID &b) const
+//      {
+//           auto key = [&](const PupuID &id) {
+//                return std::tuple{ pupuCount->at(id) == 1 ? 0 : 1,// prefer unique
+//                                   id.animation_id() == 255 ? 0 : 1,
+//                                   id.blend_mode() == BlendMode::None ? 0 : 1,
+//                                   id.animation_id() != 255 ? 0 : 1,
+//                                   id.blend_mode() != BlendMode::None ? static_cast<int>(id.blend_mode()) : -1,
+//                                   id.offset(),
+//                                   (id.is_x_not_aligned_to_grid() || id.is_y_not_aligned_to_grid()) ? 1 : 0 };
+//           };
+//           return key(a) < key(b);
+//      }
+// };
+
+// hmm since each file entry will have one or more pupu ids. would we like use the comparitor to sort the list of pupu ids and then choose the one that is first?  I plan on having like one set of keys and once one gets used up it won't be able to be used by another image. then we'll pass those images through the maskes to hide the unused pixels.hmm since each file entry will have one or more pupu ids. would we like use the comparitor to sort the list of pupu ids and then choose the one that is first?  I plan on having like one set of keys and once one gets used up it won't be able to be used by another image. then we'll pass those images through the maskes to hide the unused pixels.
