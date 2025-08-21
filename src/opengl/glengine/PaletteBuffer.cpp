aa6512441d5ccf57a08841d2e49041145666e108
Sebanisu <peeceseven@gmail.com>
2025-08-21 12:41:21 -0400

    add palettebuffer

---
+++ src/opengl/glengine/PaletteBuffer.cpp
@@ -0,0 +1,43 @@
+#include "PaletteBuffer.hpp"
+namespace glengine
+{
+void PaletteBuffer::initialize(const std::vector<glm::vec4> &data)
+{
+     if (!m_buffer_id)
+     {
+          spdlog::error("PaletteBuffer not initialized, cannot set data");
+          return;
+     }
+     glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_buffer_id);
+     glBufferData(GL_SHADER_STORAGE_BUFFER, data.size() * sizeof(glm::vec4), data.data(), GL_STATIC_DRAW);
+     glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+     m_size = data.size() / 4;// Assuming vec4 data
+     if (glGetError() != GL_NO_ERROR)
+     {
+          spdlog::error("Failed to initialize PaletteBuffer data");
+     }
+}
+
+void PaletteBuffer::bind(GLuint binding_point)
+{
+     if (m_buffer_id == 0)
+     {
+          spdlog::error("PaletteBuffer not initialized, cannot bind");
+          return;
+     }
+     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_point, m_buffer_id);
+     if (glGetError() != GL_NO_ERROR)
+     {
+          spdlog::error("Failed to bind PaletteBuffer");
+     }
+}
+
+GlidCopy PaletteBuffer::id() const
+{
+     return m_buffer_id;
+}
+size_t PaletteBuffer::size() const
+{
+     return m_size;
+}
+}// namespace glengine
